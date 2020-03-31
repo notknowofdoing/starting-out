9ac4fa37e235287abf05c7933ed730c62f44e1e0
Pro-xlr8 <agashvaran@yahoo.co.uk>
2020-03-31 23:28:07 +0100

    better comments

--- word_finder/wfnd-ag/wfnd-ag.py
+++ word_finder/wfnd-ag/wfnd-ag.py
@@ -1,25 +1,31 @@
+#0. Input
+input1 = 'books.txt' #text file that contains names of all bible books
+input2 = 'text.txt'  #text file containing the paragraph with hidden bible book names to find
+
 #1. bible books
-with open('books.txt','r') as input_file:
-    base = [line.strip() for line in input_file] #remove whitespace,'\n' and put into list with words in each line as list elements
-#print(base)
-#print(type(base))
+with open(input1,'r') as input_file:
+    list_of_books = [line.strip() for line in input_file] #remove whitespace before and after string
 
 #2. the paragraph
-with open('text.txt', 'r') as test_file:
-    test = test_file.read().lower()
+with open(input2, 'r') as test_file:
+    test = test_file.read()
+#filter(func a,iterable b) applies 'func a' to each item in b, and returns 'True' or 'False'. Then joins all 'True' values together. 
 test = ''.join(filter(str.isalpha, test)) #remove everything but alphabets
-#print(test)
-#print(type(test))
 
 #3. check if base[i] in test
 i = 1
-for x in base:
-    if str(x).lower() in test:
-        print(i,str(x))
+for book in list_of_books:
+    if str(book).lower() in test.lower():
+        print(i,str(book))
         i += 1
 
 #4. Quit
 quit = input("Press <enter> to quit:")
 
         
+#Testing
+#print(base)
+#print(type(base))
 
+#print(test)
+#print(type(test))
